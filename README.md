eababfa92664724f8cff2dc4120caef1e180abab
Liu Bing <liubinging@gmail.com>
2012-05-15 22:14:45 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Android Train System
+A brief android program project which provides user login, test and video playing.
+
+Author: Liu Bing
+E-mail: liubinging@gmail.com
+Blog:   http://unixoss.com
---
+++ src/com/unixoss/psts/PSTSActivity.java
@@ -0,0 +1,13 @@
+package com.unixoss.psts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PSTSActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
